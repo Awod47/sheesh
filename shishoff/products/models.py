354ae3b68b04e809d1515b2f7fd49be962b8f5d0
Akshay <aksh.dragon@gmail.com>
2021-10-18 22:49:49 +0530

    Third commit

--- shishoff/products/models.py
+++ shishoff/products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     Title= models.CharField(max_length= 80)
-    Image= models.ImageField(upload_to= 'media')
+    Image= models.ImageField(upload_to= 'upload/')
     Description= models.CharField(max_length= 250)
     Price= models.DecimalField(decimal_places= 2, max_digits= 6)
     date = models.DateTimeField(auto_now_add=True)
